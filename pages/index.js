e66818720b8361731089326dddd1373acfdb463b
BhavyaVadher <bhavyavadher111@gmail.com>
2022-11-02 19:05:45 +0530

    FirstCommit

---
+++ pages/index.js
@@ -0,0 +1,27 @@
+import Head from 'next/head'
+import About from '../components/About'
+import Contact from '../components/Contact'
+import Main from '../components/Main'
+import Projects from '../components/Projects'
+import Skills from '../components/Skills'
+
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Bhavya | Software Developer</title>
+        <meta name="description" content="I’m a front-end web developer specializing in building (and occasionally designing) exceptional digital experiences." />
+        <link rel="icon" href="/fav.png" />
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;1,100;1,200;1,300;1,400;1,500&display=swap" rel="stylesheet"/>
+      </Head>
+    <Main />
+    <About />
+    <Skills />
+    <Projects />
+    <Contact />
+    </div>
+  )
+}
